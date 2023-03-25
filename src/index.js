cbdf01953df2c272f0c87c74362fc0a95276657c
aaronDykes <aaron.dykes99@gmail.com>
2023-03-25 16:17:17 -0400

    asdf

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-      </Routes>
+      <App />
     </HashRouter>
   </React.StrictMode>
 )
