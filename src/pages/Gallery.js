9305551f5ededa19df959252efbb9e9822e6fab1
aaronDykes <aaron.dykes99@gmail.com>
2023-05-05 15:39:43 -0400

    Gallery is somewhat resonsive now

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,31 +1,35 @@
 import "../css/page/Gallery.scss";
 import Images from "./../images/PhotoIndex";
-
-import { Link } from "react-router-dom";
 import { BiAlignLeft } from "react-icons/bi";
+import Nav from "../components/Nav";
+import { useState } from "react";
 
 function handleImage(Image) {
   return (
-    <div key={Image._id} className="Images">
+    <div key={Image._id} className="images">
       <img
         src={Image.image}
         loading="eager"
         alt={Image.name}
-        className="Image"
+        className="image"
       ></img>
     </div>
   );
 }
 
 export default function Gallery() {
+  const [clicked, setClicked] = useState(false);
   return (
-    <div className="Gallery">
-      <div className="Menu">
-        <Link to="/" id="Head">
-          <BiAlignLeft />
-        </Link>
+    <div id="gallery">
+      <div className={`g-nav-${clicked ? "visible" : "hidden"}`}>
+        <Nav clicked={clicked} setClicked={setClicked} />
       </div>
-      <div className="Top-Content">{Images.map((el) => handleImage(el))}</div>
+      <div className={`g-content-${!clicked ? "visible" : "hidden"}`}>
+        <div className="g-icon" onClick={() => setClicked(!clicked)}>
+          <BiAlignLeft />
+        </div>
+        <div className="photos">{Images.map((el) => handleImage(el))}</div>
+      </div>
     </div>
   );
 }
