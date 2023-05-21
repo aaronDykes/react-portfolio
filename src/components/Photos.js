e17a5f5fe643f80ef4976fd98434638db88977c8
aaronDykes <aaron.dykes99@gmail.com>
2023-05-21 14:27:29 -0400

    s

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -6,7 +6,6 @@ import {
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Photos = ({ images, scrollPosition }) => {
-  images.map((el) => el).reverse();
   return images.map((el) => {
     return (
       <div className="image" key={el.name}>
